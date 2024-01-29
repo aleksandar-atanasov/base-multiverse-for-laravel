616e30c2877abdd73bb52004810190cba0b0b128
Aleksandar Atanasov <aleksandar@orangemelons.com>
2024-01-29 18:00:51 +0100

    add more unit tests

--- tests/Test.php
+++ tests/Test.php
@@ -23,22 +23,66 @@ class Test extends TestCase
     public function test_convert_positive_integer_from_base_10_to_base_62(): void
     {
         $result = $this->converter->convert(
-            input: 12345,
+            input: '100000000000',
             fromBase: 10,
             toBase: 62
         );
 
-        $this->assertEquals('3d7', strtolower($result));
+        $this->assertEquals('1L9zO9O', $result);
+    }
+
+    public function test_convert_string_from_base_62_to_base_10(): void
+    {
+        $result = $this->converter->convert(
+            input: '1L9zO9O',
+            fromBase: 62,
+            toBase: 10
+        );
+
+        $this->assertEquals('100000000000', $result);
     }
 
     public function test_convert_zero_from_base_10_to_base_62(): void
     {
         $result = $this->converter->convert(
-            input: 0,
+            input: '0',
             fromBase: 10,
             toBase: 62
         );
 
         $this->assertEquals('0', $result);
+    }
+
+    public function test_convert_zero_from_base_62_to_base_10(): void
+    {
+        $result = $this->converter->convert(
+            input: '0',
+            fromBase: 62,
+            toBase: 10
+        );
+
+        $this->assertEquals('0', $result);
+    }
+
+    public function test_it_ignores_negative_input_values_from_base_10_to_base_62(): void
+    {
+        $result = $this->converter->convert(
+            input: '-100000000000',
+            fromBase: 10,
+            toBase: 62
+        );
+
+        $this->assertEquals('1L9zO9O', $result);
+    }
+
+    public function test_it_ignores_invalid_input_chars_from_base_8_to_base_10(): void
+    {
+        $result = $this->converter->convert(
+            input: '0977567',
+            fromBase: 8,
+            toBase: 10
+        );
+
+        $this->assertEquals('32631', $result);
     }
 }
